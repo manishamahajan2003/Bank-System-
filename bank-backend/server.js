cce498f00058276f23d9a0bc950a29bf105a553c
Manisha Mahajan <132148457+manishamahajan2003@users.noreply.github.com>
2025-11-25 03:20:20 +0530

    Update server.js

--- bank-backend/server.js
+++ bank-backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(cors({
   origin: ["http://localhost:5173",
-  "https://bank-system-ejzj.vercel.app/"
+  "https://bank-system-ejzj.vercel.app"
            ],
   credentials: true
 }));
