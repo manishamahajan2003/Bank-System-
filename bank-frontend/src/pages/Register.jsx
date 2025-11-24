7855ea6aa2573b760a4b0053a5ac80ef79645e32
Manisha Mahajan <132148457+manishamahajan2003@users.noreply.github.com>
2025-11-25 02:50:32 +0530

    Update Register.jsx

--- bank-frontend/src/pages/Register.jsx
+++ bank-frontend/src/pages/Register.jsx
@@ -21,7 +21,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", form);
+      const res = await axios.post("https://bank-system-pq3p.onrender.com/api/auth/register", form);
       setMessage(res.data.message);
 
       setTimeout(() => {
